f6ef3c0af58cf4d953bae83951b328e9006501c2
itsunny01-root <contactsunny07@gmail.com>
2022-10-03 23:18:18 +0530

    Write your first code in JS

--- Javascript Part-1/Write Your first code in Js/script.js
+++ Javascript Part-1/Write Your first code in Js/script.js
@@ -1,1 +1,9 @@
+
+// Press (  Ctrl + shift + j ) in Your Browser to open console dashboards
+
 console.log('hello world');
+console.log("hello world");
+console.log(`hello world`);
+
+
+// In all way you receive the same output : hello world
