b10b6c9132c665a11fcf016ce48c997d04e06162
itsunny01-root <contactsunny07@gmail.com>
2022-10-03 23:54:57 +0530

    Variables in JS

---
+++ Javascript Part-1/variables in js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+  var Yourname = "ALOK MISHRA";
+
+// use a variable 
+
+console.log(Yourname);
+
+// change value 
+
+ Yourname = 'mishraji';
+
+console.log(Yourname);
