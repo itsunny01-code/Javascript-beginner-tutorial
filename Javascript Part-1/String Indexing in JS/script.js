a7a23e535ec6fbc6fe2c9abeeaefce59c64ac70c
itsunny01-root <contactsunny07@gmail.com>
2022-10-04 22:52:09 +0530

    string Indexing in JS

---
+++ Javascript Part-1/String Indexing in JS/script.js
@@ -0,0 +1,21 @@
+
+// String indexing 
+
+let a = "alok";
+
+
+// A     L   O   K
+//  0    1   2   3  
+
+// length of string 
+// firstName.length 
+
+console.log(a.length);  // Output : 4
+
+
+// last Index : length - 1 
+
+console.log(a[a.length-1]); // Output : k
+console.log(a[a.length-2]); // Output : o
+console.log(a[a.length-3]); // Output : l
+console.log(a[a.length-4]); // Output : a
