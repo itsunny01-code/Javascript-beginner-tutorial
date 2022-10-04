72ee206276aba4eceea9ac5d76ef26aa236643a4
itsunny01-root <contactsunny07@gmail.com>
2022-10-04 23:47:17 +0530

    methods of string in JS

---
+++ Javascript Part-1/methods of string in js/script.js
@@ -0,0 +1,44 @@
+// trim() 
+
+let b = "     sunny    ";
+
+console.log(b);
+console.log(b.length)
+
+b = b.trim(); 
+
+console.log(b);
+console.log(b.length)
+
+
+// toUpperCase()
+
+let a = 'sunny' ;
+console.log(a);
+
+a = a.toUpperCase();
+
+console.log(a);
+
+
+// toLowerCase()
+
+let d = "SDHVAKHSDV";
+
+console.log(d);
+
+d = d.toLowerCase();
+
+console.log(d);
+
+
+// slice()
+
+
+let o = "ALOK"
+console.log(o);
+
+o = o.slice(1,3);
+console.log(o);
+
+
