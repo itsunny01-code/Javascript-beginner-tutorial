1843825df680350a29291ff3ac99d501d50ad91d
itsunny01-root <contactsunny07@gmail.com>
2022-10-05 13:22:32 +0530

    Undefined - Null & BigInt in Js

---
+++ Javascript Part-1/Undefined-Null-BigInt In Js/script.js
@@ -0,0 +1,16 @@
+
+
+// undefined in Js means is the value is undefined means not defined yet
+
+let a ;
+console.log(typeof a, a); //output : undefined
+
+// null means noting 
+
+let b = null ;
+console.log(typeof b); //output : object
+
+// BigInt is used for printing the value above max safe integer
+
+let c = BigInt(4332476786252748269474597234954n);
+console.log(c);  //ouput : 4332476786252748269474597234954n
