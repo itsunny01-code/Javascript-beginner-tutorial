6e339c5749c7fad289607a12132fe650b007b9a6
itsunny01-root <contactsunny07@gmail.com>
2022-10-05 15:24:40 +0530

    if else in Js

---
+++ Javascript Part-1/If Else In Js/script.js
@@ -0,0 +1,15 @@
+
+let age = 15 ;
+
+if(age >= 18){
+    console.log('You can Vote');
+}
+
+else {
+    console.log('You Cant vote my boy');
+}
+
+
+
+// IF perform when the code is true ;
+// Else perform when the code is false ;
