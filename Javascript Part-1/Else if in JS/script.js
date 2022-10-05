2f03eae5a8ca7690c296c818e6d8c92109cf08f7
itsunny01-root <contactsunny07@gmail.com>
2022-10-05 16:40:40 +0530

    weight status game using else if in JS

---
+++ Javascript Part-1/Else if in JS/script.js
@@ -0,0 +1,35 @@
+
+
+// Lets make a game in ELSEv IF
+
+
+
+let weight = '';
+
+let user_weight = +prompt('Enter the weight');
+console.log(user_weight);
+
+if (user_weight < 10 ) {
+    console.log("EAT MOREEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+}
+
+else if (user_weight < 30 ) 
+{
+    console.log("EAT MOREEEEEEEEEEEEE");
+ }
+
+
+else if (user_weight < 50 ) 
+{
+    console.log("EAT MORE");
+ }
+
+
+else if (user_weight < 70 ) 
+{
+    console.log("FIT");
+ }
+
+ else {
+ console.log('EAT LESS');
+ }
