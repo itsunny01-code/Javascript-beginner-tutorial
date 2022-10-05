dbc0c4c2fb9f714c02ffcda38b8d22197a1859cc
itsunny01-root <contactsunny07@gmail.com>
2022-10-05 12:29:26 +0530

    Concatenation in Js

---
+++ Javascript Part-1/Concatenation in Js/script.js
@@ -0,0 +1,29 @@
+
+
+let a = "alok";
+console.log(typeof a);  // output is : string
+
+let b = 01;
+console.log(typeof b);  // output is : number
+
+
+
+// let's change there types :
+
+// string to number
+
+let aa = "alok";
+
+aa = +aa ;  // if we put  + before the variable its change there type to number
+
+console.log(typeof aa);  // output is : number
+
+
+
+// number to string
+
+let bb = 01;
+
+bb = bb + '' ;  // if we put + ''  (adding of string) after the variable its change there type to string
+
+console.log(typeof bb);  // output is : string
