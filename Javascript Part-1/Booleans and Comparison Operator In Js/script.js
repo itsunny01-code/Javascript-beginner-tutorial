8ead4e723a10889ed83ebb60373513c764eb6b4d
itsunny01-root <contactsunny07@gmail.com>
2022-10-05 13:50:58 +0530

    Booleans and comparsion operators in JS

---
+++ Javascript Part-1/Booleans and Comparison Operator In Js/script.js
@@ -0,0 +1,25 @@
+
+// Booleans always print in true or false
+
+
+// Comaparison operators
+
+// ==	equal to	
+// ===	equal value and equal type	
+// !=	not equal
+// !==	not equal value or not equal type	
+// >	greater than	
+// <	less than	
+// >=	greater than or equal to	
+// <=	less than or equal to	
+
+let a = 7 ;
+let b = 8 ;
+let c = a < b ;
+console.log(c); // output = true
+
+
+let e = 7 ;
+let f = 8 ;
+let g = a > b ;
+console.log(g); // output = false
