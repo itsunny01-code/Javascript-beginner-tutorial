2fb6714571494f3244b3626ffa6fd35c10b7f45c
itsunny01-root <contactsunny07@gmail.com>
2022-10-05 15:55:07 +0530

    logical & termary operator in js

---
+++ Javascript Part-1/Logical & Ternary operator in Js/script.js
@@ -0,0 +1,35 @@
+
+
+//ternary Operator example
+
+let agee = 19 ;
+let vote = agee>18? 'you can vote' : 'dont vote' ;
+console.log(vote);
+
+
+// And Operator Example
+
+let name = 'Alok' ;
+let weight = 65 ;
+
+if (name[0] === "A" && weight > 50  ) {
+    console.log('In');
+}
+
+else {
+    console.log('Out');
+}
+
+
+// Or Operator Example
+
+let namee = 'Alok' ;
+let weightt = 35 ;
+
+if (name[0] === "A" || weight > 50  ) {
+    console.log('In');
+}
+
+else {
+    console.log('Out');
+}
