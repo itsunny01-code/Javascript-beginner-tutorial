f8b978b690ba64f56bcccb99aff1ef5ef59516c9
itsunny01-root <contactsunny07@gmail.com>
2022-10-05 12:47:54 +0530

    String Template in Js

---
+++ Javascript Part-1/Template is Js/script.js
@@ -0,0 +1,24 @@
+// Template means printing the things in easy way :
+
+
+// Using default method
+
+let a = 'alok';
+let b = 18;
+let c = 'sultanpur';
+
+let d = "my name is"+" " +a +" " + "my age is" +" " +b +" " +"I'm from" +" " +c ;  //output is : my name is alok my age is 18 I'm from sultanpur
+
+console.log(d);
+
+
+
+// Using String Template Method
+
+let aa = 'alok';
+let bb = 18;
+let cc = 'sultanpur';
+
+let dd = `my name is ${a} my age is ${b} I'm from ${c}`;  // use one `` (sign above the tab button)
+
+console.log(d); // output is : my name is alok my age is 18 I'm from sultanpur
