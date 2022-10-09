0bce31d4f1842dfb4a1e1eeac5233c389f93c2ab
itsunny01-root <contactsunny07@gmail.com>
2022-10-09 13:58:45 +0530

    Array destruction in js

--- Javascript Part-1/Array Destruction in JS/script.js
+++ Javascript Part-1/Array Destruction in JS/script.js
@@ -4,3 +4,21 @@
  let [b ,c,d,e] = a ; // for destructing an array in separates arrays
 
  console.log(b , c, d, e);
+
+
+
+ const l = ['hi','bye','no','black'];
+
+ const [ q , ...drift] = l ; // this if you multiple value in one ARRAY and single value in one array
+
+ console.log(drift);
+
+
+
+ let p = ['no','bro','shy','gain'];
+
+ let [My ,     ,   ,  hi] = p ;  // for skipping any value
+
+ console.log(hi);
+
+
