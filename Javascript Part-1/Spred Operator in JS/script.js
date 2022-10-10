e40cf0a10f812179b5c157b22686d8860d01d1cf
itsunny01-root <contactsunny07@gmail.com>
2022-10-10 23:10:20 +0530

    Spread Operator in Object

---
+++ Javascript Part-1/Spred Operator in JS/script.js
@@ -0,0 +1,20 @@
+
+
+const ObjNew = {
+    Name : 'Alok Mishra',
+    Class : 'Last'
+}
+
+const ObjSecond = {
+    Name : 'Sunny Mishra',
+    Class : 'First'
+}
+
+const WholeObj = {...ObjNew , ObjSecond};
+
+console.log(WholeObj);
+
+
+const CreateNewObject = {...['item','item2']};
+
+console.log(CreateNewObject);
