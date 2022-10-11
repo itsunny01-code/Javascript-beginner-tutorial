d49faab5cc58708669972bbe383f17883e60a5ef
itsunny01-root <contactsunny07@gmail.com>
2022-10-12 00:17:44 +0530

    Arrow Function in JS

---
+++ Javascript Part-1/Arrow Function in JS/script.js
@@ -0,0 +1,35 @@
+
+function AddTwoNumbers(Var1 , Var2) {
+  return Var1 + Var2 ;
+}
+
+const CheckAnswer = AddTwoNumbers(4,9);
+console.log(CheckAnswer); // Simple Function
+
+
+
+
+
+
+
+const AddTwoNumberss = function(var3 , var4) {
+  return var3 + var4
+} 
+
+const CheckAnswerr = AddTwoNumberss(4,9);
+console.log(CheckAnswerr); // Function Expression 
+
+
+
+
+
+const AddTwoNumbersss = (var3 , var4) => {
+  return var3 + var4
+} 
+
+const CheckAnswerrr = AddTwoNumbersss(4,9);
+console.log(CheckAnswerr); // arrow Function 
+
+
+
+
