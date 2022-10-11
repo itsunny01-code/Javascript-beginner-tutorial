6b4d065415d00a46be0d8bb31da2a3806c6a7ba6
itsunny01-root <contactsunny07@gmail.com>
2022-10-11 23:09:07 +0530

    Function in Js

--- Javascript Part-1/Function in JS/script.js
+++ Javascript Part-1/Function in JS/script.js
@@ -1,3 +1,22 @@
-  
 
-  console
+function SayHello() {
+    console.log('Hello Mishra Ji');
+}
+
+
+SayHello();
+SayHello();
+SayHello();
+SayHello();
+SayHello();
+
+
+
+
+function SumOfTwoNumbers(v , p) {
+return v + p ;
+}
+
+const ans = SumOfTwoNumbers(400000,765464);
+console.log(ans);
+
