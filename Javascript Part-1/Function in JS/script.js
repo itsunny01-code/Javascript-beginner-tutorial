99f087a6ed2d85aa77e6003cf72c2125993f1524
itsunny01-root <contactsunny07@gmail.com>
2022-10-11 23:13:22 +0530

    Function in Js

--- Javascript Part-1/Function in JS/script.js
+++ Javascript Part-1/Function in JS/script.js
@@ -20,3 +20,9 @@ return v + p ;
 const ans = SumOfTwoNumbers(400000,765464);
 console.log(ans);
 
+
+function IsTheNumberIsGiven(Number) {
+    return Number % 2 === 0 ;
+}
+
+console.log(IsTheNumberIsGiven(6));
