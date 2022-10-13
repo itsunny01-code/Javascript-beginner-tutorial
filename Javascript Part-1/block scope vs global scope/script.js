02b70bf24f50483a41a44a764de9c2de7905aec1
itsunny01-root <contactsunny07@gmail.com>
2022-10-13 22:22:23 +0530

    Block Scope vs Global Scope

---
+++ Javascript Part-1/block scope vs global scope/script.js
@@ -0,0 +1,21 @@
+
+// {
+//     This are called blocks {} , let and const are block scope and var is global scope...
+// }
+
+
+{
+    let KeyWordsCheck = 'Heelo'
+    console.log(KeyWordsCheck);
+}
+
+console.log(KeyWordsCheck);
+
+
+
+{
+    var KeyWordsCheck = 'Heelo'
+    console.log(KeyWordsCheck);
+}
+
+console.log(KeyWordsCheck);
--- Javascript Part-1/practice/script.js
+++ Javascript Part-1/practice/script.js
@@ -1,19 +1,1 @@
-const Verfied = [
-    { User : 1 , Age : 18 , Email : 'Contactsunny07@gmail.com'},
-    { User : 11 , Age : 188 , Email : 'Contactsunny077@gmail.com'},
-    { User : 111 , Age : 1888 , Email : 'Contactsunny0777@gmail.com'},
-    { User : 111 , Age : 18888 , Email : 'Contactsunny07777@gmail.com'},
-    { User : 1111 , Age : 18888 , Email : 'Contactsunny077777@gmail.com'},
-    { User : 11111 , Age : 188888 , Email : 'Contactsunny077777@gmail.com'},
-    { User : 111111 , Age : 188888 , Email : 'Contactsunny0777777@gmail.com'},
-    { User : 1111111 , Age : 188888 , Email : 'Contactsunny077777777@gmail.com'},
-    { User : 11111111 , Age : 188888 , Email : 'Contactsunny0777777777@gmail.com'},
-]
 
-
-console.log(Verfied);
-
-
-for ( Verify of Verfied) {
-    console.log(Verify);
-}
