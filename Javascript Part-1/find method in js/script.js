c081e415b5c4cbbd31a816ed378b873a039d04d4
itsunny01-root <contactsunny07@gmail.com>
2022-10-25 23:24:07 +0530

    find method in js

---
+++ Javascript Part-1/find method in js/script.js
@@ -0,0 +1,12 @@
+const names = ['alok' , 'wedfwdfs' , 'wetqwertq3t' , 'wertfe42tgf'] ;
+
+const find = function (name) {
+    return name.length===4 ;
+}
+
+const answer = names.find(find);
+console.log(answer);
+
+
+
+// real time example soon 
