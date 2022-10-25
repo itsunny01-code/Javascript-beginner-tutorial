92f41e8a7463e4c0cdeb4b404eb77769627e1230
itsunny01-root <contactsunny07@gmail.com>
2022-10-25 23:27:14 +0530

    find method in js

--- Javascript Part-1/find method in js/script.js
+++ Javascript Part-1/find method in js/script.js
@@ -9,4 +9,18 @@ console.log(answer);
 
 
 
-// real time example soon 
+// real time example 
+
+const id  = [
+    {id : 1 , name : 'alok'},
+    {id : 2 , name : 'alokk'},
+    {id : 3 , name : 'alokkk'},
+    {id : 4 , name : 'alokkkk'}
+]
+
+
+const id_find = id.find((id)=> {
+    return id.id===1 ;
+})
+
+console.log(id_find);
