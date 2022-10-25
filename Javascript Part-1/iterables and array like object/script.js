a28cb0ac8ff2ad0c36a1f26fb9745a0ede50f673
itsunny01-root <contactsunny07@gmail.com>
2022-10-26 00:25:16 +0530

    itersbles and array like object in js

---
+++ Javascript Part-1/iterables and array like object/script.js
@@ -0,0 +1,13 @@
+// iterables are those in which we can use for loop
+
+const name = 'mynameisalokmishra';
+for(const value of name){
+    console.log(value);
+}
+
+
+// array like object is in which we can use a lenght property
+
+
+const nam = 'mynameisalokmishra';
+console.log(nam.length);
