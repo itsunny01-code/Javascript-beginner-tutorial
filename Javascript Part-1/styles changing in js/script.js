65023177bb39350d0b9bd2f4928ad87a0f553da0
itsunny01-root <contactsunny07@gmail.com>
2022-11-02 21:59:27 +0530

    Basic Styling in js

---
+++ Javascript Part-1/styles changing in js/script.js
@@ -0,0 +1,6 @@
+
+const newtext = document.querySelector(".text");
+newtext.textContent = "this is the changed text" // text changed
+console.log(newtext)
+
+newtext.style.color = "red"; // color changed
