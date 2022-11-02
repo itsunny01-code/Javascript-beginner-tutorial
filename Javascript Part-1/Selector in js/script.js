a6f189ee1e53430f357f31dcc9206792ae85cc1c
itsunny01-root <contactsunny07@gmail.com>
2022-11-02 21:54:42 +0530

    selector in js

---
+++ Javascript Part-1/Selector in js/script.js
@@ -0,0 +1,11 @@
+
+// Element bgy ID
+
+console.log(document.getElementById("text-2"));
+
+
+// Element by query selector
+
+console.log(document.querySelector(".text"));
+
+
--- Javascript Part-1/practice/script.js
+++ Javascript Part-1/practice/script.js
@@ -1,1 +1,4 @@
 
+const user = [
+    id : 
+]
